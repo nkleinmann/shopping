ca377421c8fc560c96c3ea069b9aaf217707763b
nkleinmann <nekleinmann@gmail.com>
2021-10-02 16:37:17 -0400

    Add basic homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepageComponent';
+
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
